cb2b21013e93ce9ff9b52bee90d393f9e41ed560
Tong Tang <pichip22@hotmail.com>
2023-09-17 18:34:04 -0400

    completed

--- server/app.py
+++ server/app.py
@@ -47,7 +47,26 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self,id):
+        plant = Plant.query.filter_by(id=id).first()
+        data = request.get_json()
+        for key, value in data.items():
+            setattr(plant, key, value)
+        db.session.add(plant)
+        db.session.commit()
+        plant_dict = plant.to_dict()
+        
+        response = make_response(jsonify(plant_dict), 200)
+        return response
 
+    def delete(self,id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+
+        
+        response = make_response('', 204)
+        return response
 api.add_resource(PlantByID, '/plants/<int:id>')
 
 
